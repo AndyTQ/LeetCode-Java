803efef464aef5c55946978f364f577c78b44da7
Tianquan Di <dtq99827@gmail.com>
2018-06-02 02:03:03 -0400

    Update Two_Sum.java

--- Problems/Two_Sum.java
+++ Problems/Two_Sum.java
@@ -1,13 +1,31 @@
+// Brute-force solution: O(n^2)
+// class Solution {
+//     public int[] twoSum(int[] nums, int target){
+//         int current;
+//         for (int i = 0; i < nums.length; i++){
+//             for (int j = 0; j < nums.length; j++){
+//                 if (i != j && nums[i] + nums[j] == target){
+//                     int[] result = new int[]{i, j}; 
+//                     return result;
+//                 }
+//             }
+//         }
+//         return null;
+//     }
+// }
+
+// Hash-Table solution:
 class Solution {
     public int[] twoSum(int[] nums, int target){
-        int current;
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++){
-            for (int j = 0; j < nums.length; j++){
-                if (i != j && nums[i] + nums[j] == target){
-                    int[] result = new int[]{i, j}; 
-                    return result;
-                }
+            if (map.containsKey(target - nums[i])){
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
             }
+            map.put(nums[i], i);
         }
         return null;
     }
